f210cb63c935236b7045a0e0330e1e34abe4292a
Sylvie et Lhétie <sylvie@MacBook-Pro-de-Sylvie.local>
2020-04-29 15:24:15 +0200

    Corriger de l'exercice1

---
+++ asset/js/script.js
@@ -0,0 +1,4 @@
+//je crée une variable firstname et j'attribut comme valeur une chaîne de caractères 'sylvie'
+var firstname = 'Sylvie';
+//je demande à ce que ma variable firstname s'affiche dans une boite de dialogue grace à la fonction alert()
+alert(firstname);
